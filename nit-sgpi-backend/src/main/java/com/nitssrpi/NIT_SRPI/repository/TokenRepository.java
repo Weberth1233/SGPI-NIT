8f5623373d53402a777b96eb1007b76824d834a7
Weberth1233 <weberth20-19@hotmail.com>
2026-02-27 13:23:18 -0300

    adc .env no docker-compose issue #60

--- nit-sgpi-backend/src/main/java/com/nitssrpi/NIT_SRPI/repository/TokenRepository.java
+++ nit-sgpi-backend/src/main/java/com/nitssrpi/NIT_SRPI/repository/TokenRepository.java
@@ -3,10 +3,12 @@ package com.nitssrpi.NIT_SRPI.repository;
 import com.nitssrpi.NIT_SRPI.model.PasswordResetToken;
 import com.nitssrpi.NIT_SRPI.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
+    @Transactional
     void deleteByUser(User user);
 }
